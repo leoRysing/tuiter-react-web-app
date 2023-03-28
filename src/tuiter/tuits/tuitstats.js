a898af29ed9f3982803a6367b62545c578aadad1
leoRysing <leshaigor@gmail.com>
2023-03-28 16:56:10 -0400

    adding in dislike

--- src/tuiter/tuits/tuitstats.js
+++ src/tuiter/tuits/tuitstats.js
@@ -8,9 +8,7 @@ const TuitStat = ({ tuit }) => {
   const dispatch = useDispatch();
   const onClickLike = () => {
     const liked = !tuit.liked;
-    console.log(liked);
     const likes = liked ? tuit.likes + 1 : tuit.likes - 1;
-    console.log(likes);
     dispatch(updateTuitThunk({
       ...tuit,
       likes: likes,
@@ -18,15 +16,32 @@ const TuitStat = ({ tuit }) => {
     }));
   };
 
+  // TODO: onClickDislike
+  const onClickDislike = () => {
+    const disliked = !tuit.disliked;
+    const dislikes = disliked ? tuit.dislikes + 1 : tuit.dislikes - 1;
+    dispatch(updateTuitThunk({
+      ...tuit,
+      dislikes: dislikes,
+      disliked: disliked,
+    }));
+  };
+
+  // bi-hand-thumbs-down
+
   return (
       <div className={"row w-100"}>
-        <span className={"col-3"}><i className={"bi bi-chat tuit-icon"}></i> {tuit.replies} </span>
-        <span className={"col-3"}><i className={"bi bi-arrow-repeat tuit-icon"}></i> {tuit.retuits}</span>
-        <span className={"col-3"}>
+        <span className={"col"}><i className={"bi bi-chat tuit-icon"}></i> {tuit.replies} </span>
+        <span className={"col"}><i className={"bi bi-arrow-repeat tuit-icon"}></i> {tuit.retuits}</span>
+        <span className={"col"}>
           <i onClick={onClickLike} className={`bi ${tuit.liked ? " bi-heart-fill text-danger" : "bi-heart"} me-2`}></i>
           {tuit.likes}
         </span>
-        <i className={"bi bi-share col-3"}></i>
+        <span className={"col"}>
+          <i onClick={onClickDislike} className={`bi ${tuit.disliked ? " bi-hand-thumbs-down-fill text-danger" : "bi-hand-thumbs-down"} me-2`}></i>
+          {tuit.dislikes}
+        </span>
+        <i className={"bi bi-share col"}></i>
       </div>
   )
 }
